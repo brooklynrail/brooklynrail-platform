9d0c075d17c671687b2023c5491abbb81fb12b29
Jeremy Zilar <jeremyzilar@gmail.com>
2020-10-22 15:03:50 -0600

    adding base airtable function

---
+++ themes/brooklynrail/static/netlify_functions/airtable.js
@@ -0,0 +1,58 @@
+const Airtable = require('airtable')
+const mockdata = require('./mockdata.json')
+
+exports.handler = function(event, context, callback) {
+  // Return mock
+  if (!process.env.AIRTABLE_JZ) {
+    // eslint-disable-next-line no-console
+    console.log('No key: MOCK RESPONSE')
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(mockdata),
+      headers: {
+        'content-type': 'application/json',
+        'cache-control': 'Cache-Control: max-age=60, public'
+      }
+    }
+    return callback(null, response)
+  }
+
+  Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_JZ
+  })
+
+  const base = Airtable.base('tbl0MXqMPQOo3Efad')
+  const allRecords = []
+  base('entries')
+    .select({
+      // This caps total records to 100, not just each request
+      // maxRecords: 100,
+      view: 'all'
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+          allRecords.push(record)
+        })
+        fetchNextPage()
+      },
+      function done(err) {
+        if (err) {
+          callback(err)
+        } else {
+          console.log(allRecords.length)
+          const body = JSON.stringify({ records: allRecords })
+          const response = {
+            statusCode: 200,
+            body: body,
+            headers: {
+              'content-type': 'application/json',
+              'cache-control': 'Cache-Control: max-age=60, public'
+            }
+          }
+          callback(null, response)
+        }
+      }
+    )
+}
