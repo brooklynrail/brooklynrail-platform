b60ae53014df1b2e8152d6a1a38572d68bb3425e
Jeremy Zilar <jeremyzilar@gmail.com>
2020-10-22 17:16:09 -0600

    update base and table

--- themes/brooklynrail/static/netlify_functions/airtable.js
+++ themes/brooklynrail/static/netlify_functions/airtable.js
@@ -23,8 +23,9 @@ exports.handler = function(event, context, callback) {
   })
 
   const base = Airtable.base('appw5rR1oYVuYgRKI')
+
   const allRecords = []
-  base('entries')
+  base('Immigrants')
     .select({
       // This caps total records to 100, not just each request
       // maxRecords: 100,
