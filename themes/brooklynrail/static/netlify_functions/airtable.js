aed63528960284febc1ff28cd52bdb8ec602b867
Jeremy Zilar <jeremyzilar@gmail.com>
2020-10-27 23:12:25 -0600

    trying new function

--- themes/brooklynrail/static/netlify_functions/airtable.js
+++ themes/brooklynrail/static/netlify_functions/airtable.js
@@ -1,62 +1,22 @@
-const Airtable = require('airtable')
-const mockdata = require('./mockdata.json')
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: process.env.AIRTABLE_JZ}).base('appw5rR1oYVuYgRKI');
 
-exports.handler = function(event, context, callback) {
-  // Return mock
-  if (!process.env.AIRTABLE_JZ) {
-    // eslint-disable-next-line no-console
-    console.log('No key: MOCK RESPONSE')
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(mockdata),
-      headers: {
-        'content-type': 'application/json',
-        'cache-control': 'Cache-Control: max-age=60, public'
-      }
-    }
-    return callback(null, response)
-  }
+base('Immigrants').select({
+    // Selecting the first 3 records in All Participants:
+    maxRecords: 3,
+    view: "Web Data"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
 
-  Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: process.env.AIRTABLE_JZ
-  })
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('first_name'));
+    });
 
-  const base = Airtable.base('appw5rR1oYVuYgRKI')
-  
-  const allRecords = []
-  base('Immigrants')
-    .select({
-      // This caps total records to 100, not just each request
-      // maxRecords: 100,
-      sort: [
-        {field: 'First name', direction: 'asc'}
-      ]
-    })
-    .eachPage(
-      function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-          console.log(record)
-          allRecords.push(record)
-        })
-        fetchNextPage()
-      },
-      function done(err) {
-        if (err) {
-          callback(err)
-        } else {
-          console.log(allRecords.length)
-          const body = JSON.stringify({ records: allRecords })
-          const response = {
-            statusCode: 200,
-            body: body,
-            headers: {
-              'content-type': 'application/json',
-              'cache-control': 'Cache-Control: max-age=60, public'
-            }
-          }
-          callback(null, response)
-        }
-      }
-    )
-}
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
