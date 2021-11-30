459192a979984963a96130ed2f873839d467c5fe
jeremyzilar <jeremyzilar@gmail.com>
2021-11-29 21:14:25 -0500

    update airtable sources

--- themes/brooklynrail/static/netlify_functions/getDonationData.js
+++ themes/brooklynrail/static/netlify_functions/getDonationData.js
@@ -3,15 +3,14 @@ const Airtable = require('airtable')
 exports.handler = function(event, context, callback) {
   
   var Airtable = require('airtable');
-  // The Payments In Base == appsuINlr8VpP5zPm
-  var base = new Airtable({apiKey: process.env.AIRTABLE_JZ}).base('appsuINlr8VpP5zPm');
+  // Winter Campaign Data == appvW1Ad2kMh1S1T1
+  var base = new Airtable({apiKey: process.env.AIRTABLE_JZ}).base('appvW1Ad2kMh1S1T1');
   const allRecords = []
-  // Campaigns table "tbldOazrsvPI1dh23"
-  base('tbldOazrsvPI1dh23').select({
-    // Selecting the first 3 records in All Ads:
+  // Campaigns
+  base('tbl9k6L9O6PrTtWDc').select({
     maxRecords: 1,
-    // view: "2021 Winter Campaign"
-    view: "viwmjobftcLot0jjZ",
+    // view: "All Campaigns"
+    view: "viwL1YpndNoMuNzUd",
   }).eachPage(function page(records, fetchNextPage) {
     
     records.forEach(function(record) {
