2018415c1961213d00c105ee4aeea54586b8ee96
jeremyzilar <jeremyzilar@gmail.com>
2022-11-29 10:47:30 -0500

    update campaign year

--- themes/brooklynrail/static/netlify_functions/get_checkout_session.js
+++ themes/brooklynrail/static/netlify_functions/get_checkout_session.js
@@ -56,7 +56,7 @@ exports.handler = function(event, context, callback) {
       },
       line_items: [
         {
-          name: "2021 Winter Campaign Donation",
+          name: "2022 Winter Campaign Donation",
           description: "Thank you for making a donation to the Brooklyn Rail's 2022 Winter Campaign",
           amount: data.amount,
           currency: "usd",
