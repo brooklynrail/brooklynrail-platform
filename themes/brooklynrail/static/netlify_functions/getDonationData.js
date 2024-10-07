171849a870581f89c7f0630ba32457021a5af203
jeremyzilar <jeremyzilar@gmail.com>
2024-10-07 14:32:30 -0400

    new view key

--- themes/brooklynrail/static/netlify_functions/getDonationData.js
+++ themes/brooklynrail/static/netlify_functions/getDonationData.js
@@ -11,7 +11,7 @@ exports.handler = function(event, context, callback) {
   base('tbl9k6L9O6PrTtWDc').select({
     maxRecords: 1,
     // This ID should match the view that is named to the current year
-    view: "viwJQuIYtRgQVqJbD",
+    view: "viwvOf3TJixFFjjkn",
   }).eachPage(function page(records, fetchNextPage) {
     
     records.forEach(function(record) {
